e9f3bf3b0d600009d605871977f299798ce23e8c
Vai <rivai.ramandhani@gmail.com>
2019-10-30 10:41:08 +0700

    Updates

---
+++ precache-manifest.115bb806017a10541b66f1b8507a9dc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df00ca127a62ef1fed8061299adb46a8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0518cce319802a10625f",
+    "url": "/monsters-rolodex/static/css/main.36263cc5.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "0518cce319802a10625f",
+    "url": "/monsters-rolodex/static/js/main.a9da8e0b.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
